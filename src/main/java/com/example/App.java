aca69f15268da54d9179657b13977772fa9aaefb
Meghaab04 <meghaagowda16@gmail.com>
2026-04-28 12:16:44 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Application started!");
+
+        while (true) {
+            System.out.println("Running...");
+            Thread.sleep(5000);
+        }
+    }
+}
